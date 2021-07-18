7517a762a7494779ed81496ce5e0a13009f0d8a9
bhanderi akshay <akshay.bhanderi45678@gmail.com>
2021-07-18 21:06:25 +0530

    ok

--- lab01c.py
+++ lab01c.py
@@ -5,7 +5,19 @@ import matplotlib.pyplot as plt
 from google.colab import drive
 drive.mount('/content/drive')
 
-data=pd.read_csv('/content/drive/My Drive/Colab Notebooks/ML-Lab/L1/mtcars.csv') 
-d=pd.crosstab(index=data['cyl'],columns="count",dropna=True ) 
+data=pd.read_csv('/content/drive/MyDrive/Study/ML/Lab1/mtcars.csv')
+d=pd.crosstab(index=data['cyl'],columns="count",dropna=True)
 print(d)
 
+print(data.info())
+
+print("Total Null Data:",data.isnull().sum())
+
+plt.hist(data['mpg'],bins=5)
+plt.show()
+
+plt.scatter(data['mpg'],data['wt'])
+plt.show()
+
+df=pd.DataFrame(data,columns=['gear'])
+print("Count How Many values:\n",df['gear'].value_counts())
